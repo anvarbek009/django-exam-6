ce6bc6bad9e20f1d5c17d773c53118ccb7a00a10
anvarbek009 <anvarasqarov2009@gmail.com>
2024-05-22 15:57:27 +0500

    commit

--- conf/urls.py
+++ conf/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('', include('home.urls')),
     path('clocks/', include('clocks.urls')),
     path('users/', include('users.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
