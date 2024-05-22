7459b7dd15eef6ed475f52cb3db09d37f372bd12
anvarbek009 <anvarasqarov2009@gmail.com>
2024-05-22 17:13:25 +0500

    commit

--- clocks/admin.py
+++ clocks/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Review,Watch
+from .models import Review,Watch,Cart,CartItem
 # Register your models here.
 
 admin.site.register(Review)
 admin.site.register(Watch)
+admin.site.register(Cart)
+admin.site.register(CartItem)
 
